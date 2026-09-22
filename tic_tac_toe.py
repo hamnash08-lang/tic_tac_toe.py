d6ca407e94007faebf47b28c85c64af299b9f561
hamnash08-lang <hamnash08@gmail.com>
2026-09-22 21:16:49 +0500

    Add initial game board and display function
    Initialize the Tic-Tac-Toe game board and display function.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,1 +1,14 @@
+# Tic-Tac-Toe Game
 
+board = [" ", " ", " ",
+         " ", " ", " ",
+         " ", " ", " "]
+
+def display_board():
+    print()
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("--+---+--")
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("--+---+--")
+    print(board[6] + " | " + board[7] + " | " + board[8])
+    print()
